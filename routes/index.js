d179a59e6bcb92955eaeb9828fc8479fef213370
Maxence Bouret <maxence.bouret@gmail.com>
2018-11-02 15:59:06 +0100

    Add book done

--- routes/index.js
+++ routes/index.js
@@ -40,4 +40,31 @@ router.get('/books/:id', (req, res, next) => {
     })
 })
 
+// The route to display the form
+router.get('/add-book', (req, res, next) => {
+  res.render('add-book')
+})
+
+// The route to handle the form
+router.post('/add-book', (req, res, next) => {
+  // If there is an empty title
+  if (!req.body.title) {
+    res.render('add-book', {
+      error: "The title must be filled"
+    })
+    return
+  }
+
+  // Create a book in the db with the information from the form
+  Book.create({
+    title: req.body.title, // the title entered in "<input type="text" name="title">"
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating,
+  })
+    .then(book => {
+      res.redirect('/books/' + book._id)
+    })
+})
+
 module.exports = router;
